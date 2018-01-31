ef4895a3d37c2e999c41d1e8576cbd460ab080c9
Santiago Ottonello <sanotto@gmail.com>
2018-01-31 23:08:48 +0000

    fixed error in validation $this missing a dollar sign

--- src/Attributes/CUITAttribute.php
+++ src/Attributes/CUITAttribute.php
@@ -52,65 +52,65 @@ class CUITAttribute extends Attribute
         // Check for valid ip string
         $strvalue = Tools::atkArrayNvl($record, $this->fieldName(), '');
         if ($strvalue != '' && $strvalue != '...') 
-				{
- 						if(!this::isValidCUIT($strvalue))
-						{
+		{
+ 		    if(!$this::isValidCUIT($strvalue))
+			{
                 Tools::triggerError($record, $this->fieldName(), 'CUIT No v&aacute;lido');
-						}
+			}
         }
         parent::validate($record, $mode);
     }
 
-		public static function isValidCUIT($cuit)
-		{
-			$coeficiente[0]=5;
-			$coeficiente[1]=4;
-			$coeficiente[2]=3;
-			$coeficiente[3]=2;
-			$coeficiente[4]=7;
-			$coeficiente[5]=6;
-			$coeficiente[6]=5;
-			$coeficiente[7]=4;
-			$coeficiente[8]=3;
-			$coeficiente[9]=2;
+	public static function isValidCUIT($cuit)
+	{
+		$coeficiente[0]=5;
+		$coeficiente[1]=4;
+		$coeficiente[2]=3;
+		$coeficiente[3]=2;
+		$coeficiente[4]=7;
+		$coeficiente[5]=6;
+		$coeficiente[6]=5;
+		$coeficiente[7]=4;
+		$coeficiente[8]=3;
+		$coeficiente[9]=2;
  
-			$resultado=1;
+		$resultado=1;
  
-			for ($i=0; $i < strlen($cuit); $i= $i +1) 
-			{    //separo cualquier caracter que no tenga que ver con numeros
-				if ((Ord(substr($cuit, $i, 1)) >= 48) && (Ord(substr($cuit, $i, 1)) <= 57))
-				{
-					$cuit_rearmado = $cuit_rearmado . substr($cuit, $i, 1);
-				}
-			}
- 
-			if (strlen($cuit_rearmado) <> 11) 
-			{  // si to estan todos los digitos
-					return false;
-			} 
-			else 
+		for ($i=0; $i < strlen($cuit); $i= $i +1) 
+        {   
+            //separo cualquier caracter que no tenga que ver con numeros
+			if ((Ord(substr($cuit, $i, 1)) >= 48) && (Ord(substr($cuit, $i, 1)) <= 57))
 			{
-				$sumador = 0;
-				$verificador = substr($cuit_rearmado, 10, 1); //tomo el digito verificador
- 
-				for ($i=0; $i <=9; $i=$i+1) 
-				{
-					$sumador = $sumador + (substr($cuit_rearmado, $i, 1)) * $coeficiente[$i];//separo cada digito y lo multiplico por el coeficiente
-				}
- 
-				$resultado = $sumador % 11;
-				$resultado = 11 - $resultado;  //saco el digito verificador
-				$veri_nro = intval($verificador);
- 
-				if ($veri_nro <> $resultado) 
-				{
-					return false;
-				} 
-				$cuit_rearmado = substr($cuit_rearmado, 0, 2) . "-" . substr($cuit_rearmado, 2, 8) . "-" . substr($cuit_rearmado, 10, 1);
-				return true;
-			}
+			    $cuit_rearmado = $cuit_rearmado . substr($cuit, $i, 1);
+            }	
 		}
 
+        if (strlen($cuit_rearmado) <> 11) 
+        {  
+            // si to estan todos los digitos
+			return false;
+		} 
+		else 
+		{
+			$sumador = 0;
+			$verificador = substr($cuit_rearmado, 10, 1); //tomo el digito verificador
+ 			for ($i=0; $i <=9; $i=$i+1) 
+			{
+				$sumador = $sumador + (substr($cuit_rearmado, $i, 1)) * $coeficiente[$i];//separo cada digito y lo multiplico por el coeficiente
+			}
+
+			$resultado = $sumador % 11;
+			$resultado = 11 - $resultado;  //saco el digito verificador
+			$veri_nro = intval($verificador);
+
+			if ($veri_nro <> $resultado) 
+			{
+				return false;
+			} 
+			$cuit_rearmado = substr($cuit_rearmado, 0, 2) . "-" . substr($cuit_rearmado, 2, 8) . "-" . substr($cuit_rearmado, 10, 1);
+			return true;
+		}
+	}
     /**
      * Converts the internal attribute value to one that is understood by the
      * database.
